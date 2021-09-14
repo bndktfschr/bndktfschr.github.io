e0d25ebe035ebc4bf173e116f4a2f2387911d48b
Benedikt Fischer <89687390+bndktfschr@users.noreply.github.com>
2021-09-14 15:38:34 +0200

    Updated

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// No Internet Explorer Support!
+
+if(/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
+    window.location = 'microsoft-edge:' + window.location;
+    setTimeout(function() {
+      window.location = 'https://go.microsoft.com/fwlink/?linkid=2135547';
+    }, 1);
+}
+
+
+// PreLoader fadeout by 100% loaded
+$(window).on("load",function(){
+    $(".loader-wrapper").delay(1200).fadeOut("slow");
+});
